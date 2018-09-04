2e9c2d8d8398ac569d2ae27a8e447b7750b0a20e
stivendiaz <stivendiazagudelo@gmail.com>
2018-09-03 22:08:45 -0500

    Implementacion de modal para crear pregunta con AJAX y JavaScript

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -7,6 +7,21 @@ class PostsController < ApplicationController
     @post = Post.new
   end
 
+  def create
+    @post = Post.new(post_params)
+    if @post.save
+      redirect_to posts_path, notice: "El post fue publicado con éxito"
+    else
+      render :new
+    end
+  end
+
   def show
   end
+
+  private
+    def post_params
+      params.require(:post).permit(:title, :description)
+    end
+
 end
