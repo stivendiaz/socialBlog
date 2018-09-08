3236f512ee3a66b6d2e0d0daca91e74a2fa5b2f4
stivendiaz <stivendiazagudelo@gmail.com>
2018-09-07 19:47:29 -0500

    Asocia usuarios a posts

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,7 +1,3 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
-
-  def private_access
-    redirect_to :new_user_session unless user_signed_in?
-  end
 end
