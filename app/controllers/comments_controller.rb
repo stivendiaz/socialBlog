f6f576ae9bbfbe07db8e3b64750eb6ee0505ddbc
stivendiaz <stivendiazagudelo@gmail.com>
2018-09-09 13:34:16 -0500

    todo funciona menos las alertas

---
+++ app/controllers/comments_controller.rb
@@ -0,0 +1,12 @@
+class CommentsController < ApplicationController
+require 'pry'
+  def create
+    post = Post.find(params[:post_id])
+    post.comments.create(comments_params)
+    redirect_to post , notice: "El comentario fue publicado con éxito"
+  end
+  private
+    def comments_params
+      params.require(:comment).permit(:body).merge(user: current_user)
+    end
+end
--- app/models/post.rb
+++ app/models/post.rb
@@ -12,7 +12,8 @@
 
 class Post < ApplicationRecord
   belongs_to :user
-
+  has_many :comments
   validates :title, presence: true
   validates :description, presence: true
+  # validates :description, length: { minimum: 250 }
 end
