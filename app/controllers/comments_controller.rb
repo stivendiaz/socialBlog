06ccf4a375ef7d10b46a9aaf922250576f448b7d
stivendiaz <stivendiazagudelo@gmail.com>
2018-09-14 18:22:10 -0500

    se termino de organizar estilos y alertas

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -2,9 +2,9 @@ class CommentsController < ApplicationController
 require 'pry'
   def create
     post = Post.find(params[:post_id])
-    post.comments.create(comments_params)
     redirect_to post , notice: "El comentario fue publicado con éxito"
   end
+
   private
     def comments_params
       params.require(:comment).permit(:body).merge(user: current_user)
--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -9,7 +9,7 @@ Devise.setup do |config|
   # Devise will use the `secret_key_base` as its `secret_key`
   # by default. You can change it below and use your own secret key.
   # config.secret_key = '40c7ebaad23a4059b5fa52c1af9c49040dc54f93022b305a793e404d8b4a061337a0c427365406949e383e70e09639f85bc048d50d8b3c22552626661aa7d1fc'
-  
+
   # ==> Controller configuration
   # Configure the parent class to the devise controllers.
   # config.parent_controller = 'DeviseController'
@@ -229,7 +229,7 @@ Devise.setup do |config|
   # Turn scoped views on. Before rendering "sessions/new", it will first check for
   # "users/sessions/new". It's turned off by default because it's slower if you
   # are using only default views.
-  # config.scoped_views = false
+  config.scoped_views = true
 
   # Configure the default scope given to Warden. By default it's the first
   # devise role declared in your routes (usually :user).
